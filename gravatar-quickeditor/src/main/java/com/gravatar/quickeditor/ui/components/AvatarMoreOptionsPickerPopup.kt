f5ea4d02902b31b33f025942a60c91b2e0050eeb
Adam Grzybkowski <agrzybkowski@outlook.com>
2024-12-12 14:12:53 +0100

    QuickEditor: Avatar Rating - part 1 (#486)
    * Implement nested menu in the PickerPopup
    
    * Show Avatar Rating nested menu in the popup
    
    * Pass AvatarOption.Rating when rating changed
    
    * Optimizee /Users/adam/workspace/Gravatar-SDK-Android/gravatar-quickeditor/src/main/res/drawable/gravatar_avatar_more_options_rating.xml
    
    * Use full Avatar.Rating name from the string resources

--- gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/AvatarMoreOptionsPickerPopup.kt
+++ gravatar-quickeditor/src/main/java/com/gravatar/quickeditor/ui/components/AvatarMoreOptionsPickerPopup.kt
@@ -1,5 +1,6 @@
 package com.gravatar.quickeditor.ui.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -7,14 +8,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.gravatar.quickeditor.R
+import com.gravatar.quickeditor.ui.avatarpicker.AvatarRating
+import com.gravatar.restapi.models.Avatar
 import com.gravatar.ui.GravatarTheme
 
 @Composable
 internal fun AvatarMoreOptionsPickerPopup(
+    avatarRating: List<AvatarRating>,
     anchorAlignment: Alignment.Horizontal,
     offset: DpOffset,
     onDismissRequest: () -> Unit,
@@ -24,41 +29,73 @@ internal fun AvatarMoreOptionsPickerPopup(
         anchorAlignment = anchorAlignment,
         offset = offset,
         onDismissRequest = onDismissRequest,
-        popupItems = listOf(
-            PickerPopupItem(
-                text = R.string.gravatar_qe_selectable_avatar_more_options_alt_text,
-                iconRes = R.drawable.gravatar_avatar_more_options_alt_text,
-                contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_alt_text_content_description,
-                onClick = {
-                    onAvatarOptionClicked(AvatarOption.ALT_TEXT)
-                },
-            ),
-            PickerPopupItem(
-                text = R.string.gravatar_qe_selectable_avatar_more_options_download_image,
-                iconRes = R.drawable.gravatar_avatar_more_options_download,
-                contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_download_image,
-                onClick = {
-                    onAvatarOptionClicked(AvatarOption.DOWNLOAD_IMAGE)
-                },
-            ),
-            PickerPopupItem(
-                text = R.string.gravatar_qe_selectable_avatar_more_options_delete,
-                iconRes = R.drawable.gravatar_avatar_more_options_delete,
-                contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_delete_content_description,
-                color = MaterialTheme.colorScheme.error,
-                onClick = {
-                    onAvatarOptionClicked(AvatarOption.DELETE)
-                },
+        popupMenu = PickerPopupMenu(
+            items = listOf(
+                PickerPopupItem(
+                    text = stringResource(R.string.gravatar_qe_selectable_avatar_more_options_alt_text),
+                    iconRes = R.drawable.gravatar_avatar_more_options_alt_text,
+                    contentDescription =
+                        R.string.gravatar_qe_selectable_avatar_more_options_alt_text_content_description,
+                    onClick = {
+                        onAvatarOptionClicked(AvatarOption.AltText)
+                    },
+                ),
+                PickerPopupItem(
+                    text = stringResource(R.string.gravatar_qe_selectable_avatar_more_options_rating),
+                    iconRes = R.drawable.gravatar_avatar_more_options_rating,
+                    contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_rating,
+                    subMenu = PickerPopupMenu(
+                        items = avatarRating.map { (rating, selected) ->
+                            PickerPopupItem(
+                                text = stringResource(rating.fullNameRes),
+                                iconRes = if (selected) R.drawable.ic_checkmark else null,
+                                contentDescription = R.string.gravatar_qe_avatar_rating_selected_content_description,
+                                onClick = {
+                                    onAvatarOptionClicked(AvatarOption.Rating(rating))
+                                },
+                            )
+                        },
+                    ),
+                ),
+                PickerPopupItem(
+                    text = stringResource(R.string.gravatar_qe_selectable_avatar_more_options_download_image),
+                    iconRes = R.drawable.gravatar_avatar_more_options_download,
+                    contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_download_image,
+                    onClick = {
+                        onAvatarOptionClicked(AvatarOption.DownloadImage)
+                    },
+                ),
+                PickerPopupItem(
+                    text = stringResource(R.string.gravatar_qe_selectable_avatar_more_options_delete),
+                    iconRes = R.drawable.gravatar_avatar_more_options_delete,
+                    contentDescription = R.string.gravatar_qe_selectable_avatar_more_options_delete_content_description,
+                    contentColor = MaterialTheme.colorScheme.error,
+                    onClick = {
+                        onAvatarOptionClicked(AvatarOption.Delete)
+                    },
+                ),
             ),
         ),
     )
 }
 
-internal enum class AvatarOption {
-    ALT_TEXT,
-    DELETE,
-    DOWNLOAD_IMAGE,
+internal sealed class AvatarOption {
+    data object AltText : AvatarOption()
+
+    data class Rating(val rating: Avatar.Rating) : AvatarOption()
+
+    data object Delete : AvatarOption()
+
+    data object DownloadImage : AvatarOption()
 }
+
+private val Avatar.Rating.fullNameRes: Int
+    @StringRes get() = when (this) {
+        Avatar.Rating.G -> R.string.gravatar_qe_avatar_rating_g
+        Avatar.Rating.PG -> R.string.gravatar_qe_avatar_rating_pg
+        Avatar.Rating.R -> R.string.gravatar_qe_avatar_rating_r
+        Avatar.Rating.X -> R.string.gravatar_qe_avatar_rating_x
+    }
 
 @Preview
 @Composable
@@ -70,6 +107,7 @@ private fun AvatarMoreOptionsPickerPopupPreview() {
                 .background(MaterialTheme.colorScheme.background),
         ) {
             AvatarMoreOptionsPickerPopup(
+                avatarRating = emptyList(),
                 anchorAlignment = Alignment.Start,
                 offset = DpOffset.Zero,
                 onDismissRequest = {},
